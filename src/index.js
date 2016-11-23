b374358cf78e1fa0d0a916bfa3287bfdf587a5fa
Bill Gottsch <billgottsch@gmail.com>
2016-11-23 11:40:24 -0600

    appends work with city name

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-function loadIt(e) {
+function giveMe(e) {
   e.preventDefault();
 
   var cityReq = $('.cityReq').val();
@@ -10,11 +10,14 @@ function loadIt(e) {
         url:"http://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(cityReq) + "&APPID=1a1576ef47c8c1b185e9e0cadda52974"
       }).done(function(data){
           var group = $('<div class="group"></div>')
-          if (data.current.name === "undefined") {
+          if (data.name === "undefined") {
             alert ('Sorry we can\'t find a city by that name!')
           } else {
-            $.each(data,function(index, res){
-              group.append($('<br>' + '<h1 class="cityTitle"></h1>').text(city.name.weather));
+            $.each(data,function(request, result){
+              group.append($('<br>' + '<h1 class="cityTitle"></h1>').text(data.name));
+              group.append($('<br>' + '<div class="cityWeather__temp"></div>').text(Math.round(data.main.temp*9/5 - 459.67)+ '°F'));
+              group.append($('<br>' + '<div class="cityWeather__humidity"></div>').text('Humidity: ' + data.main.humidity + '%'));
+
             })
           }
           $('.content').empty().append(group);
@@ -22,6 +25,6 @@ function loadIt(e) {
 });
 }
 }
-$('.search').on("submit", loadIt);
+$('.search').on("submit", giveMe);
 // Bill's API key for storage
 //"api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1a1576ef47c8c1b185e9e0cadda52974",
